6e0f9bc2f006f3414e8b7a0f06ebbf2d93e67d1d
Geoffrey Challen <challen@illinois.edu>
2023-02-03 11:47:37 -0600

    Bump dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ import java.util.Properties
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.cs124-illinois"
-version = "2023.1.0"
+version = "2023.2.0"
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.10"
     java
     `maven-publish`
 
@@ -15,7 +15,7 @@ plugins {
     checkstyle
     id("com.github.sherter.google-java-format") version "0.9"
 
-    id("com.github.ben-manes.versions") version "0.44.0"
+    id("com.github.ben-manes.versions") version "0.45.0"
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 repositories {
@@ -24,8 +24,8 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
 }
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
-    implementation("io.github.classgraph:classgraph:4.8.153")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+    implementation("io.github.classgraph:classgraph:4.8.154")
     implementation("io.github.kostaskougios:cloning:1.10.3")
 
     testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
