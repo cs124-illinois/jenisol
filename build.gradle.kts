da19b7ca0150753c5b19b3e83b8dfc0c755a900e
Geoffrey Challen <challen@illinois.edu>
2024-11-01 05:24:36 -0500

    Bump version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.util.Properties
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.cs124"
-version = "2024.10.0"
+version = "2024.11.0"
 
 plugins {
     kotlin("jvm") version "2.0.21"
